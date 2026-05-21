08a67360a0124a36d21c0297342f48efcb4aebf1
Jed Oloo <154731595+Odiedo123@users.noreply.github.com>
2026-05-21 18:51:54 +0000

    Adding another model

--- static/js/script.js
+++ static/js/script.js
@@ -248,3 +248,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }, { passive: false });
 });
+
+const cursorDot = document.querySelector('.custom-dot-cursor');
+
+window.addEventListener('mousemove', (e) => {
+  cursorDot.style.left = e.clientX + 'px';
+  cursorDot.style.top = e.clientY + 'px';
+});
