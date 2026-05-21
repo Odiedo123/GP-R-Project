e05cfbbd172a173d8e72eab4941c117268440afb
Jed Oloo <154731595+Odiedo123@users.noreply.github.com>
2026-05-21 17:38:56 +0000

    Adding rwandan traditional symbols

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template, request
-from dotenv import load_dotenv
+from flask import Flask, render_template, request # type: ignore
+from dotenv import load_dotenv # type: ignore
 
 
 load_dotenv("keys.env")
